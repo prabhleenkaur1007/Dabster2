f492c78f8603538fda10c07076906b0114ffef75
Prabhleen Kaur <43867458+prabhleenkaur1007@users.noreply.github.com>
2019-09-10 16:35:42 +0530

    done routes

---
+++ routes/home/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router() ; 
+const Object = require('../../models/Object');
+
+router.all ('/*' , (req, res, next)=>{ // router.all represents that every type of request is acceptable
+    // '/*' means that every request that will come from admin/ will be accepted 
+
+    req.app.locals.layout = 'home'; // this will override the home layout with home layout 
+    next();
+});
+/////////////////////////////////////////////////////////////////
+router.get("/",function(req, res){
+	res.render("../index.html");
+})
+
+
+//////////////////////////////////////////////////////////////////
+
+router.get('/', (req, res)=>{
+    Object.find({}).then ( objects=> {
+        res.render('home/index',{objects : objects});
+    }).catch(err=>{
+        console.log(err);
+    })
+})
+
+module.exports = router ; 
+
+
